5c8603547339f220eb2584fa210a59de49cefa31
saim00191 <saimamjad198@gmail.com>
2024-06-06 21:57:46 +0500

    Add Good Evening

--- DAY37/Q109.js
+++ DAY37/Q109.js
@@ -2,3 +2,6 @@ var currentTime = new Date();
 if (currentTime.getTime() < 12) {
     console.log("GOOD MORNING!");
 }
+else {
+    console.log("Good Evening");
+}
--- DAY37/Q109.ts
+++ DAY37/Q109.ts
@@ -3,4 +3,6 @@ const currentTime = new Date()
 
 if (currentTime.getTime() < 12) {
     console.log("GOOD MORNING!");
+} else {
+    console.log("Good Evening");
 }
